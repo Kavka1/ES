be1c34f1beeabdcd951403be00b5f609759d93da
Xu Kang <xukang_0317@163.com>
2021-12-06 14:57:00 +0800

    fix demo

--- antithetic_es/demo.py
+++ antithetic_es/demo.py
@@ -18,7 +18,7 @@ def load_exp(result_path: str) -> Dict:
 
 def create_arguments() -> Dict:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--exp_result_path', type=str, default='results/Walker2d-v2_12-06_14-46')
+    parser.add_argument('--exp_path', type=str, default='results/Walker2d-v2_12-06_14-46')
     parser.add_argument('--num_rollout', type=int, default=50)
     args = parser.parse_args()
     args = vars(args)
